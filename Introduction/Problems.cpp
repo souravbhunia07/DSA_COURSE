e89cccaaf0737a361178bca788853f5e6c4ddb48
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2023-05-31 02:29:57 +0530

    update

--- Introduction/Problems.cpp
+++ Introduction/Problems.cpp
@@ -4,41 +4,41 @@ using namespace std;
 int main()
 {
     //1. Calculate the average of five number.
-    // int num1 = 23;
-    // int num2 = 34;
-    // int num3 = 54;
-    // int num4 = 12;
-    // int num5 = 34;
+    int num1 = 23;
+    int num2 = 34;
+    int num3 = 54;
+    int num4 = 12;
+    int num5 = 34;
 
-    // int sum = num1 + num2 + num3 + num4 + num5;
+    int sum = num1 + num2 + num3 + num4 + num5;
 
-    // double average = (double)sum / 5;
+    double average = (double)sum / 5;
 
-    // cout << average << endl;
+    cout << average << endl;
 
     // 2. Calculate the area of a circle
-    // const double pi = 3.14159;
-    // double radius;
+    const double pi = 3.14159;
+    double radius;
 
-    // cout << "Enter the radius of a circle: ";
-    // cin >> radius;
+    cout << "Enter the radius of a circle: ";
+    cin >> radius;
 
-    // double area = pi * radius * radius;
+    double area = pi * radius * radius;
 
-    // cout << "The area of the circle: " << area << endl;
+    cout << "The area of the circle: " << area << endl;
 
     // 3. Calculate the sum of two number
-    // int num1, num2;
+    int num1, num2;
 
-    // cout << "Enter the First Number: ";
-    // cin >> num1;
+    cout << "Enter the First Number: ";
+    cin >> num1;
 
-    // cout << "Enter the Second Number: ";
-    // cin >> num2;
+    cout << "Enter the Second Number: ";
+    cin >> num2;
 
-    // int sum = num1 + num2;
+    int sum = num1 + num2;
 
-    // cout << "The sum of two number is: " << sum << endl;
+    cout << "The sum of two number is: " << sum << endl;
 
     // 4. Guess the Number
     srand(time(0));  // to change number every time we run the program
