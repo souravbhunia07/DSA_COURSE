9207acf68ff11e0d493d89461cf2908353c31ead
Sourav Kumar Bhunia <souravbhunia400@gmail.com>
2023-06-04 21:34:06 +0530

    update

--- Control_Structures/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-cout << "Multiplication table of " << number << "(While Loop): " << endl;
-    // int k = 1;
-    // while (k <= 10)
-    // {
-    //     int result = number * k;      
-    //     cout << number << " * " << k << " = " << result << endl;
-    //     k++;
-    // }
